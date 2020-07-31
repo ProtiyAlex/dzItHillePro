d36eafb9e6343d9f12446af20fbdca1b087adec5
Khramov Alex <protiyalex@gmail.com>
2020-07-31 08:08:15 +0300

    dz_24 new

--- dz_24/webpack.config.js
+++ dz_24/webpack.config.js
@@ -9,9 +9,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   devtool: "inline-source-map",
-  optimization: {
-    minimize: true,
-  },
   module: {
     rules: [
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
